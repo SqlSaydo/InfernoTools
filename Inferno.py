5e048bb9a91c03068e096034d04e1325beb955c4
SqlSaydo <piffoiffutazei9000@gmail.com>
2024-09-18 19:10:20 +0200

    Add files via upload

---
+++ Inferno.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+def ouvrir_lien():
+    url = "https://discord.gg/nxls"
+    webbrowser.open(url)
+
+if __name__ == "__main__":
+    ouvrir_lien()
---
+++ README.md
@@ -0,0 +1,4 @@
+Le tools est encore en développement. Il sortira entre le 22 septembre et le 28 septembre !!
+
+https://discord.gg/nxls
+https://discord.gg/freebackup
